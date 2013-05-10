97b72857c6a667f89ed2b5e83680303b562e5034
Oignons <atome.corporation@gmail.com>
2013-05-10 17:53:14 +0200

    Changing console.log() at when the server is starting.

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ var server = require('./Server');
 var globals = require('./globals.js');
 
 //read configuration file
+console.log("Reading configuration...");
 var data = fs.readFileSync('config.json');
 config = JSON.parse(data);
+console.log("\tDone.");
 
 //read configuration for css file
+console.log("Reading system files....")
 CONFIG_css_index_of = fs.readFileSync(config.system_pages.css_index_of);
-
-console.log(config);
+console.log("\tDone.");
 
 var s = new server.Server(config.http_listen_port);
 s.start();
