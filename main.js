c407853890351a8c39b5c59755bd49d1b244b63b
Oignons <atome.corporation@gmail.com>
2013-05-10 15:38:34 +0200

    Read a new file at the start.

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var globals = require('./globals.js');
 var data = fs.readFileSync('config.json');
 config = JSON.parse(data);
 
+//read configuration for css file
+CONFIG_css_index_of = fs.readFileSync(config.system_pages.css_index_of);
+
 console.log(config);
 
 var s = new server.Server(config.http_listen_port);
