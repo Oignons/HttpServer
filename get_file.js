379d378430f6efbff0dc5a1ff93f6561829476b4
Oignons <atome.corporation@gmail.com>
2013-05-10 15:36:40 +0200

    Function in another file.

--- get_file.js
+++ get_file.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
-var mmm = require('mmmagic');
 var globals = require('./globals.js');
+var indexof = require('./gen_index_of.js')
 
 //Get a file
 function getFile(file, callback) {
@@ -27,77 +27,12 @@ function errorHandle(err, path, callback) {
 	}
 	//If it's a directory
 	if (err.code = 'EISDIR') {
-		genDirPage(path, callback)
+		indexof.genIndexOf(path, callback) //Generate index page and show it
 		return;
 	}
 	else {
 		callback(err.code);
 		return;
-	}
-}
-
-//Generates the page we see for a directory (list of files,...) if there is no default index file in
-function genDirPage(path, callback) {
-	//Add / at the end or not, to avoids bugs like "dir1dir2" instead of "dir1/dir2"
-	var current_dir = path.replace(RegExp(config.web_directory), '');
-	if (current_dir[current_dir.length-1] != '/') current_dir += '/';
-	if (path[path.length-1] != '/') path += '/';
-
-	//Check if default file exists
-	fs.exists(path+config.default_index_name, function(exists_bool){
-		//If yes, just show it
-		if (exists_bool) {
-			getFile(path+config.default_index_name, callback);
-		}
-		//If no, just show directory (if it's enabled)
-		else if (config.show_directory) {
-			fs.readdir(path, function(err, files) {
-				if (err) console.log(err);
-
-				var generated = "<html><head><title>Directory</title></head><body>";
-
-				generated += "<h1>Index of " + path + "</h1>";
-
-				//Root dir before
-				generated += "<a href='/'>/</a><br/>";
-
-				detectMIMETypes(path, files, [], 0, function(mimes_types) {
-					for (var i=0; i<files.length; i++) {
-						if (mimes_types[i] == config.directory_mime_type) {
-							generated += "<a href='" + current_dir + files[i] + "'>" + files[i] + "/</a><br/>";
-						}
-						else {
-							generated += "<a href='" + current_dir + files[i] + "'>" + files[i] + "</a><br/>";
-						}
-					}
-					generated += "</body></html>";
-					callback(generated);
-				});
-			});
-		}
-		else {
-			getFile(config.system_pages.http403, function(content) {
-				callback(content);
-			});
-		}
-	});
-}
-
-//Detect the MIME types in order to add a '/' at the end of the name of a directory
-function detectMIMETypes(path, files, mimes_types, i, callback) {
-	console.log(path+files[i]);
-	if (i < files.length) {
-		var magic = new mmm.Magic(mmm.MAGIC_MIME_TYPE);
-		magic.detectFile(path+files[i], function(err, result) {
-			if (err) mimes_types[i] = 'unknown';
-			else {
-				mimes_types[i] = result;
-				detectMIMETypes(path, files, mimes_types, i+1, callback);
-			}
-		});
-	}
-	else {
-		callback(mimes_types);
 	}
 }
 
